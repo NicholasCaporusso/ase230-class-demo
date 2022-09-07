91dbf685ea79997d32548289e15b76038a19220f
Nicholas Caporusso <ncaporusso@gmail.com>
2022-09-07 16:38:24 -0400

    I am satisfied of my work

---
+++ detail.php
@@ -0,0 +1,11 @@
+<?php
+//DETAIL
+$beatles=[
+	['name'=>'John Lennon'],
+	['name'=>'Paul McCartney'],
+	['name'=>'Ringo Starr'],
+	['name'=>'George Harrison'],
+	['name'=>'Elton John']
+];
+?>
+<h1><?= $beatles[$_GET['id']]['name'] ?></h1>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//INDEX
+$beatles=[
+	['name'=>'John Lennon'],
+	['name'=>'Paul McCartney'],
+	['name'=>'Ringo Starr'],
+	['name'=>'George Harrison'],
+	['name'=>'Elton John']
+];
+$i=0;
+foreach($beatles as $member){
+	echo '<h1>'.$member['name'].'</h1>';
+	echo '<a href="detail.php?id='.$i.'&name='.$member['name'].'">View details</a>';
+	$i++;
+}
+echo '<hr />';
+
+$i=0;
+foreach($beatles as $member){
+	?>
+	<h1><?= $i ?>. <?= $member['name'] ?></h1>
+	<a href="detail.php?id=<?= $i ?>&name=<?= $member['name'] ?>">View details</a>
+	<?php
+	$i++;
+}
