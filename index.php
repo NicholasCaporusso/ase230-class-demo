45b2e1038f42d3635f8afa8a71274de1e96015b7
Nicholas Caporusso <ncaporusso@gmail.com>
2022-09-12 15:53:51 -0400

    CHanged to ase230 students

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
 
-
-
-
-
-
-//INDEX
-$beatles=[
-	['name'=>'John Lennon'],
-	['name'=>'Paul McCartney'],
-	['name'=>'Ringo Starr'],
-	['name'=>'George Harrison'],
-	['name'=>'Elton John']
+$ase230_students=[
 ];
+
 $i=0;
-foreach($beatles as $member){
+foreach($ase230_students as $member){
 	echo '<h1>'.$member['name'].'</h1>';
 	echo '<a href="detail.php?id='.$i.'&name='.$member['name'].'">View details</a>';
 	$i++;
@@ -22,7 +12,7 @@ foreach($beatles as $member){
 echo '<hr />';
 
 $i=0;
-foreach($beatles as $member){
+foreach($ase230_students as $member){
 	?>
 	<h1><?= $i ?>. <?= $member['name'] ?></h1>
 	<a href="detail.php?id=<?= $i ?>&name=<?= $member['name'] ?>">View details</a>
